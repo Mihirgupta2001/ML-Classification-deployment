915af4f9586e2a8e08216d2825ac446d188956b4
Mihirgupta2001 <mihirgupta>
2023-04-10 20:21:48 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-#from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation
 #from src.components.data_transformation import DataTransformationConfig
 
 #from src.components.model_trainer import ModelTrainerConfig
@@ -25,8 +25,15 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\Cardiovascular_risk_clean.csv')
+            df=pd.read_csv('notebook\data\data_cardiovascular_risk.csv')
             logging.info('Read the dataset as dataframe')
+            
+            
+            df['is_smoking'] = df['is_smoking'].astype('category').cat.codes
+            df['sex'] = df['sex'].astype('category').cat.codes
+
+            logging.info('Converting string to categorical variable')
+
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
@@ -51,4 +58,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
