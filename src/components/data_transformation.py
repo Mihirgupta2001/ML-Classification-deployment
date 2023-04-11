9fe4d330ad4a670eaf9bbaefa67a07dc6c8ccada
Mihirgupta2001 <mihirgupta>
2023-04-11 15:58:30 +0530

    Created pickle file

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -103,10 +103,8 @@ class DataTransformation:
 
 
             smote = SMOTE()
-            input_feature_train_df, target_feature_train_df = smote.fit_resample(input_feature_train_df, 
-                                                                                 target_feature_train_df)
-            input_feature_test_df, target_feature_test_df = smote.fit_resample(input_feature_test_df, 
-                                                                                 target_feature_test_df)
+            input_feature_train_df, target_feature_train_df = smote.fit_resample(input_feature_train_df, target_feature_train_df)
+            input_feature_test_df, target_feature_test_df = smote.fit_resample(input_feature_test_df, target_feature_test_df)
             
             logging.info(f"Smote applied successfully")
 
@@ -114,10 +112,8 @@ class DataTransformation:
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[
-                input_feature_train_arr,0,0, np.array(target_feature_train_df)
-            ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            train_arr = np.r_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.r_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
 
