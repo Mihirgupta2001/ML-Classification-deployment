cdaa56c4e7d71f4b9b3cc2f6ba1c298414647bf9
Mihir Gupta <112897995+Mihirgupta2001@users.noreply.github.com>
2023-04-19 02:28:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -37,13 +37,13 @@ def predict_datapoint():
             
         )
         pred_df = data.get_data_as_data_frame()
-        print(pred_df)
+        #print(pred_df)
 
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        if results[0] == 1.0:
+        if results[0] == '1.0':
             return render_template('home.html',results = "You are at a Risk of Chronic heart diesease")
-        elif results[0] == 0.0:
+        elif results[0] == '0.0':
             return render_template('home.html',results = "Hooray!!! You are Fit and Fine")
         
     
